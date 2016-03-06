445de4095f670a108820b11ccb8e399791333538
Jacob Shomstein <jacob@shomstein.com>
2016-03-05 23:01:57 -0500

    Edits... Uploading to Firebase in JSON format.
    not done yet. Added refrence to firebase and more

--- alphatrend.net/assets/js/advertiser.js
+++ alphatrend.net/assets/js/advertiser.js
@@ -1,19 +1,41 @@
 $(document).ready(function() {
 	Materialize.toast("<p>We believe in modern simplicity and ease, upload your block <br> quickly and easily</p>", 1000, 'teal rounded');
 	//Get all the variables
-	var name = $('#business_name').val(),
-			email = $('#business_email').val(),
-			phone = $('#business_phone').val(),
-			website = $('#business_wesbite').val(),
-			//block
-			featured = $('#block_featured').val(),
-			section = $('input[name=block_section]'),
-			text = $('#block_text').val(),
-			link = $('#block_link').val();
+
 
 			//Firebase refrence
 			var firebase = new Firebase('https://alphatrend.firebaseio.com');
 
-			firebase.set("yo" : {yo : "yo"});
-			console.log(name + email + phone);
+			//Test
+			//firebase.set({'bo' :{yo :"yo"}});
+			//console.log(name + email + phone);
+
+			$('#pay').click(function() {
+				console.log('pay clicked!');
+
+				firebase.set({'bo' :{yo :"yo"}});
+
+				var name = $('#business_name').val(),
+						email = $('#business_email').val(),
+						phone = $('#business_phone').val(),
+						website = $('#business_wesbite').val(),
+						//block
+						featured = $('#block_featured').val(),
+						section = $('input[name=block_section]'),
+						text = $('#block_text').val(),
+						link = $('#block_link').val();
+
+				//SEND TO FIREBASE
+				firebase.set({[name] : {
+					"email" : [email],
+					"phone" : [email],
+					"website" : [phone],
+					"block" : {
+						"featured" : [featured],
+						"section" : [section],
+						"text" : [text],
+						"link" : [link]
+					}
+				}});
+			});
 });
