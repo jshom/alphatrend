4ac20abd8f6cfae037d7d5ceafd7a5b7f9f85e6e
Jacob Shomstein <jacob@shomstein.com>
2016-03-12 23:05:06 -0500

    Tons of edits
    Index changes, Firebase upload edits *not complete, charging stripe php
    (node later)

--- alphatrend.net/assets/js/advertiser.js
+++ alphatrend.net/assets/js/advertiser.js
@@ -13,29 +13,37 @@ $(document).ready(function() {
 			$('#pay').click(function() {
 				console.log('pay clicked!');
 
-				firebase.set({'bo' :{yo :"yo"}});
+
+
+
 
 				var name = $('#business_name').val(),
 						email = $('#business_email').val(),
 						phone = $('#business_phone').val(),
 						website = $('#business_wesbite').val(),
-						//block
 						featured = $('#block_featured').val(),
 						section = $('input[name=block_section]'),
 						text = $('#block_text').val(),
 						link = $('#block_link').val();
 
-				//SEND TO FIREBASE
-				firebase.set({[name] : {
+				var business = {
 					"email" : [email],
-					"phone" : [email],
-					"website" : [phone],
+					"phone" : [phone],
+					"website" : [website],
 					"block" : {
 						"featured" : [featured],
 						"section" : [section],
 						"text" : [text],
 						"link" : [link]
 					}
-				}});
+				};
+				//SEND TO FIREBASE
+
+				firebase.set({[name] : {email : [email], phone: [phone], website: [website]}});
+
+				console.log(business);
+				//firebase.set({business});
+
+
 			});
 });
