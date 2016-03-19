23e74365d86e14efa74055fa8e75ae15a9128e9f
Jacob Shomstein <jacob@shomstein.com>
2016-03-19 18:38:22 -0400

    Details Changed

--- alphatrend.net/charge.php
+++ alphatrend.net/charge.php
@@ -8,10 +8,10 @@ $token = $_POST['stripeToken'];
 // Create the charge on Stripe's servers - this will charge the user's card
 try {
   $charge = \Stripe\Charge::create(array(
-    "amount" => 1000, // amount in cents, again
+    "amount" => 5000, // amount in cents, again
     "currency" => "usd",
     "source" => $token,
-    "description" => "Example charge"
+    "description" => "Block on Alphatrend.net"
     ));
 } catch(\Stripe\Error\Card $e) {
   // The card has been declined
