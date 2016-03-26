b2c1b1d5164559481a4db97b2915e21bcc8852ce
Jacob Shomstein <jacob@shomstein.com>
2016-03-26 18:12:57 -0400

    Changes for payment

--- alphatrend.net/assets/js/scripts.js
+++ alphatrend.net/assets/js/scripts.js
@@ -4,37 +4,21 @@ $(document).ready(function(){
 	}, function() {
 		$('#thedot').remove();
 	});
-	
+
 	//Materialize.toast('CODE JACOB! CODE!', 4000);
-	
+
 	$('.logo').hide().fadeIn();
-	
+
 	$(".button-collapse").sideNav();
-	
+
 	var windsize = $(window).width();
-	
+
 	Materialize.toast("Hi, the dev wishes you a nice day", 4000);
-	
+
 	if ($(window).width() < 1100) {
 		$('.links').css('border-right', 'none');
 	} else {
-		
+
 	}
-	
-	//Syncing with Firebase
-	
-	var ref = new Firebase("https://alphatrend.firebaseio.com/");
-	
-	//get data from form and send via firebase
-	
-	var zcompname = "pikwik tech";
-	
-	// For Firebase make sure you use the [] to sorround the variables.  For example [dashit]
-	
-	ref.set({[zcompname] : { lastname : "shomstein", email : "sometin@sklfds.com" } });
-	
-	ref.on('value', function(snapshot) {
-		console.log(snapshot.val());
-	});
-	
+
 });
--- alphatrend.net/charge.php
+++ alphatrend.net/charge.php
@@ -1,24 +1,39 @@
 <?php
+  //requires
 
-$comment = "STRIPE PAYMENT";
+  require_once('stripe');
 
-// Set your secret key: remember to change this to your live secret key in production
-// See your keys here https://dashboard.stripe.com/account/apikeys
-\Stripe\Stripe::setApiKey("sk_test_BQokikJOvBiI2HlWgH4olfQ2");
 
-// Get the credit card details submitted by the form
-$token = $_POST['stripeToken'];
+  echo "Your block has been paid for and is now being verified for publishing, the block should be online within 24 hours";
+  if(isset($_POST)) {
+    //general details
+    $img = $_POST['b_name'];
 
-// Create the charge on Stripe's servers - this will charge the user's card
-try {
-  $charge = \Stripe\Charge::create(array(
-    "amount" => 5000, // amount in cents, again
-    "currency" => "usd",
-    "source" => $token,
-    "description" => "Block on Alphatrend.net"
+    echo $img;
+  } else {
+    exit("sorry, an error has occured");
+  }
+
+  $comment = "STRIPE PAYMENT";
+
+  // Set your secret key: remember to change this to your live secret key in production
+  // See your keys here https://dashboard.stripe.com/account/apikeys
+  \Stripe\Stripe::setApiKey("sk_test_BQokikJOvBiI2HlWgH4olfQ2");
+
+  // Get the credit card details submitted by the form
+  $token = $_POST['stripeToken'];
+
+  // Create the charge on Stripe's servers - this will charge the user's card
+  try {
+    $charge = \Stripe\Charge::create(array(
+      "amount" => 5000, // amount in cents, again
+      "currency" => "usd",
+      "source" => $token,
+      "description" => "Block on Alphatrend.net"
     ));
-} catch(\Stripe\Error\Card $e) {
-  // The card has been declined
-}
+    echo('all good');
+  } catch(\Stripe\Error\Card $e) {
+    // The card has been declined
+  }
 
 ?>
